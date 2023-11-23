e1bbdc9b82182371dccd43033e4d63c2b6eabb54
mikljohsn <114925102+mikljohsn@users.noreply.github.com>
2023-11-23 13:10:23 +0100

    added db fetch

--- pages/addCourse/addCourse.js
+++ pages/addCourse/addCourse.js
@@ -1,17 +1,45 @@
 
 
-const API_URL = "localhost:3306/api/courses"
+const API_URL = "http://localhost:8080/api/"
 
 
-export function initAddCourse(){
+/*export function initAddCourse(){
     const token = localStorage.getItem('token');
     if (token === null) {
         alert("You must be logged in to change courses")
         window.router.navigate("#")
         return;
     }
+loadPlanes()
 document.querySelector("#addCourse").addEventListener("click", async () => addCourse())
-}
+}*/
+
+loadPlanes()
+
+async function loadPlanes(){
+    console.log("i was c<lled")
+    let airplanes = document.querySelector("#airplaneId")
+    try {
+        const response = await fetch(API_URL + "airplanes");
+        
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+    
+        const data = await response.json();
+        
+        
+        data.forEach(item => {
+          let option = document.createElement('option');
+          option.value = item.id;
+          option.text = item.airplaneType;
+          airplanes.appendChild(option);
+        });
+      } catch (error) {
+        console.error('There was a problem fetching the data:', error);
+      }
+    };
+    document.querySelector("#addCourse").addEventListener("click", async () => addCourse())
 
 async function addCourse(){
     const form = document.querySelector("#carForm")
@@ -35,7 +63,7 @@ async function addCourse(){
     };
 
     try {
-        const response = await fetch(API_URL, options);
+        const response = await fetch(API_URL +"courses", options);
         const courseResponse = await response.json();
         console.log('Course added:', courseResponse);
         // Optionally, do something with the response
@@ -44,3 +72,5 @@ async function addCourse(){
         // Handle errors, show an error message to the user, etc.
     }
 };
+
+
