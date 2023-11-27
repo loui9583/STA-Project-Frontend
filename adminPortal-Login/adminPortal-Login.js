4beab8e8ca7bca99901ba5b498a95fe8db4ca51e
Hoejhus <mikk57k0@stud.kea.dk>
2023-11-27 12:56:56 +0100

    adminportal update
    changes to login and created admin dashboard (missing functionality)

--- adminPortal-Login/adminPortal-Login.js
+++ adminPortal-Login/adminPortal-Login.js
@@ -1,24 +1,24 @@
 import { API_URL} from "../settings.js";
 
 export function initLogin() {
-  document.getElementById("login-form").style.display = "block";
+  document.getElementById("loginForm").style.display = "block";
 
   const loginForm = document.getElementById("loginForm");
 
-  loginForm.addEventListener("submit", async (e)=> {
+  loginForm.addEventListener("submit", async (e) => {
     e.preventDefault();
-    login()
-});
+    login();
+  });
 }
 
 async function login() {
     const formData = new FormData(loginForm);
     const username = formData.get("username");
     const password = formData.get("password");
-  
+
     try {
       const response = await fetch(
-        API_URL + "/auth/adminPortal-login",
+        API_URL + "/auth/admin-portal-login",
         makeOptions("POST", { username, password }, false)
       );
   
@@ -34,6 +34,8 @@ async function login() {
         localStorage.setItem("token", token);
         localStorage.setItem("username", loggedInUser);
         localStorage.setItem("roles", roles);
+
+        window.router.navigate("/adminPortal")
       } else {
         loggedInMessage.textContent =
           "Login failed. Please check your credentials.";
@@ -44,3 +46,19 @@ async function login() {
     }
   }
 
+  export function makeOptions(method, body, addToken) {
+    const opts = {
+      method: method,
+      headers: {
+        "Content-type": "application/json",
+        "Accept": "application/json"
+      }
+    }
+    if (body) {
+      opts.body = JSON.stringify(body);
+    }
+     if (addToken && localStorage.getItem("token")) {
+      opts.headers.Authorization = "Bearer " + localStorage.getItem("token")
+    }
+    return opts;
+  }
